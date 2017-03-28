37fd1512f1770076d6142f50e79a26c900c0b6eb
Jake Nelson <jnelson180@gmail.com>
2017-03-27 20:31:51 -0500

    Added reac-chartjs dependency

--- app/src/components/ClicksContainer.jsx
+++ app/src/components/ClicksContainer.jsx
@@ -10,38 +10,38 @@ module.exports = React.createClass({
 	getInitialState: function(){
 		return {
 			clicks: undefined
-		} 
+		}
 	},
 	componentWillMount: function () {
 		ajaxRequest ('GET', apiUrl, function(data){
 			this.setState({
 				clicks: JSON.parse(data).clicks
-			}) 
-		}.bind(this)) 
+			})
+		}.bind(this))
 	},
 	render: function() {
-		return ( 
+		return (
 			<div className="container">
 				<Header />
-				<p>You have clicked the button {this.state.clicks} times.</p>   
+				<p>You have clicked the button {this.state.clicks} times.</p>
 				<br />
 				<div className="btn-container">
 					<Btn className="btn-add" onClick={this.handleAddClick} text="CLICK ME!" />
 					<Btn className="btn-delete" onClick={this.handleDeleteClick} text="RESET" />
 				</div>
-			</div>			
+			</div>
 		)
 	},
 	handleAddClick: function() {
 		ajaxRequest ('POST', apiUrl, function(data){
 			console.log(data);
 			this.setState({clicks: JSON.parse(data).clicks})
-		}.bind(this))		
+		}.bind(this))
 	},
 	handleDeleteClick: function () {
 		ajaxRequest ('DELETE', apiUrl, function(data){
 			console.log('delete');
 			this.setState({clicks: JSON.parse(data).clicks})
-		}.bind(this))			
+		}.bind(this))
 	}
-}); 
+});
