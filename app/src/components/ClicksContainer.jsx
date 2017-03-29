5be4605c8c53a05ca275166898704ab1c69d187b
Jake Nelson <jnelson180@gmail.com>
2017-03-28 23:01:47 -0500

    Refactored ClicksContainer.js to ES6 React class and beautified code

--- app/src/components/ClicksContainer.jsx
+++ app/src/components/ClicksContainer.jsx
@@ -9,53 +9,49 @@ var clicksApiUrl = window.location.origin + '/api/:id/clicks';
 var pollDataApiUrl = window.location.origin + '/polldata/api';
 
 module.exports = class extends React.Component {
-	constructor(props) {
-	super(props);
-	this.state = {res: undefined};
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            res: undefined
+        };
+    }
 
-		componentDidMount() {
-		ajaxRequest ('GET', pollDataApiUrl, function(data){
-			console.log('1 going to ' + pollDataApiUrl)
-			var result = JSON.parse(data);
-				this.setState({
-					res: result
-				})
-			}.bind(this))
-	}
+    componentDidMount() {
+        ajaxRequest('GET', pollDataApiUrl, function(data) {
+            console.log('1 going to ' + pollDataApiUrl)
+            var result = JSON.parse(data);
+            this.setState({res: result})
+        }.bind(this))
+    }
 
-	render() {
-		if(this.state.res) {
-		console.log(this.state.res.pollInfo);
+    render() {
+        if (this.state.res) {
+            console.log(this.state.res.pollInfo);
 
-		return (
+            return (
 
-			<div className="container">
-				<Header />
-				<p></p>
-				<br />
-				<div className="polls-container">
-					  <SinglePoll data={this.state.res.pollInfo.votes}
-					labels={this.state.res.pollInfo.fields} question={this.state.res.pollInfo.question}/>
-				</div>
-			</div>
-		)
-	}
-	else {
-		return (
-			<div className="container">
-				<Header />
-				<p></p>
-				<br />
-				<div className="polls-container">
+                <div className="container">
+                    <Header/>
+                    <p></p>
+                    <br/>
+                    <div className="polls-container">
+                        <SinglePoll data={this.state.res.pollInfo.votes} labels={this.state.res.pollInfo.fields} question={this.state.res.pollInfo.question}/>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className="container">
+                    <Header/>
+                    <p></p>
+                    <br/>
+                    <div className="polls-container"></div>
+                </div>
+            )
+        }
+    }
 
-				</div>
-			</div>
-		)
-	}
-	}
-
-	/*
+    /*
 		handleAddClick() {
 			ajaxRequest ('POST', clicksApiUrl, function(data){
 				console.log(data);
