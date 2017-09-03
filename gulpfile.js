f4a4b85400adccdac5aa7650bb4c350d0a401b3a
Jake Nelson <jnelson180@gmail.com>
2017-09-02 21:13:11 -0500

    linted

--- gulpfile.js
+++ gulpfile.js
@@ -9,57 +9,57 @@ var notifier = require('node-notifier');
 var livereload = require('gulp-livereload');
 var babelify = require('babelify');
 
-var notify = function(error) {
-  var message = 'In: ';
-  var title = 'Error: ';
+var notify = function (error) {
+    var message = 'In: ';
+    var title = 'Error: ';
 
-  if(error.description) {
-    title += error.description;
-  } else if (error.message) {
-    title += error.message;
-  }
+    if (error.description) {
+        title += error.description;
+    } else if (error.message) {
+        title += error.message;
+    }
 
-  if(error.filename) {
-    var file = error.filename.split('/');
-    message += file[file.length-1];
-    console.log(message);
-  }
+    if (error.filename) {
+        var file = error.filename.split('/');
+        message += file[file.length - 1];
+        console.log(message);
+    }
 
-  if(error.lineNumber) {
-    console.log('error On Line: ' + error.lineNumber);
-    console.log(error);
-  }
-  console.log(error.description + ":\n" + error.message ? error.message : "");
-  notifier.notify({title: title, message: message});
+    if (error.lineNumber) {
+        console.log('error On Line: ' + error.lineNumber);
+        console.log(error);
+    }
+    console.log(error.description + ":\n" + error.message ? error.message : "");
+    notifier.notify({ title: title, message: message });
 };
 
 var bundler = watchify(browserify({
-  entries: ['./app/src/App.jsx'],
-  transform: [babelify],
-  extensions: ['.jsx', '.js'],
-  debug: true,
-  cache: {},
-  packageCache: {},
-  fullPaths: true
+    entries: ['./app/src/App.jsx'],
+    transform: [babelify],
+    extensions: ['.jsx', '.js'],
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
 }));
 
 function bundle() {
-  return bundler
-    .bundle()
-    .on('error', notify)
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./public/'))
-    .pipe(livereload());
+    return bundler
+        .bundle()
+        .on('error', notify)
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('./public/'))
+        .pipe(livereload());
 }
 bundler.on('update', bundle)
 
-gulp.task('build', function() {
-  bundle()
+gulp.task('build', function () {
+    bundle()
 });
 
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('./app/src/**', ['build']);
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch('./app/src/**', ['build']);
 });
 
 gulp.task('default', ['build', 'watch']);
